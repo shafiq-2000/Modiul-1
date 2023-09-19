d2dc05bd9cfe1a55fc1df12cfc8e59cd071a108f
shafiq-2000 <shafiq53471@gmail.com>
2023-09-19 14:39:22 +0600

    new line code

--- index.php
+++ index.php
@@ -5,12 +5,7 @@
 
     <?php
     $number = 5;
-    // if ($number > 10) {
 
-    //     echo "$number is a larg";
-    // } else {
-    //     echo " $number very small number";
-    // }
     $list=($number>10? 'it is larg number': 'otherswise small number');
     echo "inthe case of $list";
     ?>
